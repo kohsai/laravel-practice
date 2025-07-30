471c3852e15101a81ac96af1a89efa1e51c2b007
kohsai <saishokoudai@gmail.com>
2025-07-30 09:02:17 +0900

    ルーティング微調整

--- src/laravel/routes/web.php
+++ src/laravel/routes/web.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PageController
-;
+use App\Http\Controllers\PageController;
 
 Route::get('/', [HomeController::class,'index']);
 
